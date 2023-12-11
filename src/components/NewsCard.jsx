6582a2df59da3a9c842bf07f64cd101cf0baee15
Hashim El-Alej <hashimmelalej@gmail.com>
2023-12-11 15:44:05 +0000

    improved articles

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
 
-{/* <header className='display-header'>
-                <h2>Top Headlines:</h2>
-            </header> */}
-
 function NewsCard({ article }) {
-    return (
-        <>
-        {/* <img id='football' src={article.article_img_url} alt="Football Image"/> */}
-            <article className='news-card'>
-                <h3 id='news-title'>{article.title}</h3>
-                <p id='news-paragraph'>{article.body}</p>
-                <button>Read More</button>
-            </article>
-        </>
-    );
+  return (
+    <>
+      <article className='news-card'>
+        <div className='picture-icon'>
+          <img id='icon' src={article.article_img_url} alt="Football Image" />
+        </div>
+        <div>
+          <h3 id='news-title'>{article.title}</h3>
+          <p id='news-paragraph'>{article.body}</p>
+          <button>Read More</button>
+        </div>
+      </article>
+    </>
+  );
 }
 
 export default NewsCard;
