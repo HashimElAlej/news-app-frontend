013116607bfa2375952adda8e484626032ab727f
Hashim El-Alej <hashimmelalej@gmail.com>
2023-12-12 14:22:02 +0000

    comments

--- src/components/ArticleDisplay.jsx
+++ src/components/ArticleDisplay.jsx
@@ -1,16 +1,26 @@
 import { fetchArticleById } from "../api";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import CommentsCard from "./CommentCard";
 
 
 function ArticleDisplay({ article, setArticle }) {
+
+  const [comments, setComments] = useState([]);
   const { article_id } = useParams();
-  
+
   useEffect(() => {
     fetchArticleById((article_id))
-    .then((data) => {
-      setArticle(data[0])
-    })
+      .then((data) => {
+        setArticle(data[0])
+      })
+  }, [article_id]);
+
+  useEffect(() => {
+    fetchAllComments((article_id))
+      .then((data) => {
+        setComments(data)
+      })
   }, [article_id]);
 
   return (
@@ -21,14 +31,21 @@ function ArticleDisplay({ article, setArticle }) {
       </div>
 
       <div className='white-background'>
-       <p>Author: {article.author}</p>
-       <p>Date: {article.created_at}</p>
+        <p>Author: {article.author}</p>
+        <p>Date: {article.created_at}</p>
       </div>
 
       <img id='icon-2' src={article.article_img_url} alt="Football Image" />
 
       <div className='white-background'>
         <p>{article.body}</p>
+      </div>
+
+      <div>
+        <h3>Comments:</h3>
+        {comments.map((comment) => {
+          return <CommentsCard key={comment.comment_id} comment={comment} />
+        })}
       </div>
 
     </article>
